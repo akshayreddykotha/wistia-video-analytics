9759cfaa71e3078a94930d181aa7cdb19c30b8e6
rsm-akotha <akotha@ucsd.edu>
2025-08-05 13:55:42 -0700

    deploying wistia visitors extract lambda

--- ingestion/ingest_wistia_visitors.py
+++ ingestion/ingest_wistia_visitors.py
@@ -6,7 +6,7 @@ from io import StringIO, BytesIO
 from datetime import datetime
 
 # Wistia API constants
-BASE_URL = "https://api.wistia.com/v1/medias"
+BASE_URL = "https://api.wistia.com/v1/stats/visitors.json"
 WISTIA_API_TOKEN = os.getenv("WISTIA_API_TOKEN")
 if not WISTIA_API_TOKEN:
     raise Exception("Missing WISTIA_API_TOKEN environment variable")
@@ -14,9 +14,9 @@ if not WISTIA_API_TOKEN:
 # AWS S3 bucket
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "ak-wistia")  # fallback if not set
 
-def fetch_all_media_metadata(per_page=100, max_pages=3):
+def fetch_all_visitors(per_page=100, max_pages=3):
     page = 1
-    all_media = []
+    all_visitors = []
 
     headers = {
         "Authorization": f"Bearer {WISTIA_API_TOKEN}"
@@ -34,15 +34,15 @@ def fetch_all_media_metadata(per_page=100, max_pages=3):
             print(f"Error: {response.status_code} - {response.text}")
             break
 
-        media_batch = response.json()
-        if not media_batch:
+        visitors_batch = response.json()
+        if not visitors_batch:
             print("No more data to fetch. Exiting.")
             break
 
-        all_media.extend(media_batch)
+        all_visitors.extend(visitors_batch)
         page += 1
 
-    return all_media
+    return all_visitors
 
 def upload_json_to_s3(data, bucket, filename):
     s3 = boto3.client("s3")
@@ -54,16 +54,16 @@ def upload_json_to_s3(data, bucket, filename):
 
 def lambda_handler(event, context):
     try:
-        media_data = fetch_all_media_metadata()
+        visitors_data = fetch_all_visitors()
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = f"raw_data/media/media_metadata_{timestamp}.json"
+        filename = f"raw_data/visitors/visitors_data_{timestamp}.json"
 
-        upload_json_to_s3(media_data, S3_BUCKET_NAME, filename)
+        upload_json_to_s3(visitors_data, S3_BUCKET_NAME, filename)
 
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": f"Fetched {len(media_data)} records and uploaded to S3.",
+                "message": f"Fetched {len(visitors_data)} records and uploaded to S3.",
                 "s3_key": filename
             })
         }
