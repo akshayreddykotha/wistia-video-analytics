60fee0fb79901ff1544ff2f06b8adee9e8e3c1a0
rsm-akotha <akotha@ucsd.edu>
2025-08-10 16:45:26 -0700

    implementing scd 1 on dim_media table

--- transformations/scripts/glue_dim_media.py
+++ transformations/scripts/glue_dim_media.py
@@ -19,7 +19,23 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # ====== Read Raw JSON from S3 ======
-raw_path = f"s3://{source_bucket}/raw_data/media/"
+metadata_key = f"s3://ak-wistia/raw_data/media/s3_keys/latest_media_file.json"  # example key where the pointer is stored
+
+# Initialize boto3 S3 client
+s3 = boto3.client("s3")
+
+# Read the metadata file content from S3 (no local temp file)
+obj = s3.get_object(Bucket=source_bucket, Key=metadata_key)
+content = obj['Body'].read().decode('utf-8')
+
+# Parse JSON to get the latest file path (assuming JSON structure like {"latest_s3_key": "raw_data/media/filename.json"})
+metadata = json.loads(content)
+latest_file_key = metadata.get("latest_s3_key")
+if not latest_file_key:
+    raise Exception("latest_s3_key not found in metadata JSON")
+
+# Full S3 path to latest JSON file - Implementing SCD1
+latest_file_s3_path = f"s3://{source_bucket}/{latest_file_key}"
 df_raw = spark.read.json(raw_path, multiLine=True)
 
 # ====== Flatten & Select Required Columns ======
