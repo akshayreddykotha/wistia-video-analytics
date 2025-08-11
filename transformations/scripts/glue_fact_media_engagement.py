19a4401d9fe12b211e5490113adc87b11698bb67
rsm-akotha <akotha@ucsd.edu>
2025-08-11 14:09:44 -0700

    getting the table from gluecontext - loading the data into dimensional model to create fact_media_engagement

--- transformations/scripts/glue_fact_media_engagement.py
+++ transformations/scripts/glue_fact_media_engagement.py
@@ -1,7 +1,8 @@
 import sys
 from awsglue.utils import getResolvedOptions
-from pyspark.sql import SparkSession
+from awsglue.context import GlueContext
 from pyspark.sql.functions import col, current_date, round
+from pyspark.context import SparkContext
 
 # ====== Parse Job Parameters ======
 args = getResolvedOptions(sys.argv, [
@@ -13,35 +14,34 @@ args = getResolvedOptions(sys.argv, [
 target_bucket = args['TARGET_BUCKET']
 glue_db = args['GLUE_DATABASE']
 
-# ====== Create Spark Session ======
-spark = SparkSession.builder \
-    .appName("FactMediaEngagementETL") \
-    .enableHiveSupport() \
-    .getOrCreate()
+# ====== Create Spark & Glue Context ======
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
 
 # ====== Read dim_media from Glue Catalog ======
-dim_media_df = spark.sql(f"""
-    SELECT hashed_id, title, duration
-    FROM {glue_db}.dim_media
-""")
+dim_media_df = glueContext.create_dynamic_frame.from_catalog(
+    database=glue_db,
+    table_name="dim_media"
+).toDF().select("hashed_id", "title", "duration")
 
 # ====== Read fact_events from Glue Catalog ======
-fact_events_df = spark.sql(f"""
-    SELECT media_id, percent_viewed, visitor_key, event_key, received_at
-    FROM {glue_db}.fact_events  
-""")
+fact_events_df = glueContext.create_dynamic_frame.from_catalog(
+    database=glue_db,
+    table_name="fact_events"
+).toDF().select("media_id", "percent_viewed", "visitor_key", "event_key", "received_at")
 
 # ====== Join and Calculate Watch Time ======
 fact_media_engagement_df = fact_events_df.join(
     dim_media_df,
-    fact_events_df.media_id == dim_media_df.hashed_id,    
+    fact_events_df.media_id == dim_media_df.hashed_id,
     how="inner"
 ).withColumn(
     "watch_time_seconds",
     round(col("percent_viewed") / 100 * col("duration"), 2)
 ).withColumn(
     "load_date", current_date()
-).drop(dim_media_df.hashed_id) 
+).drop(dim_media_df.hashed_id)
 
 # ====== Write to S3 as Parquet ======
 target_path = f"s3://{target_bucket}/processed/fact_media_engagement/"
