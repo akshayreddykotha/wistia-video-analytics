acf1882f29a76228b4208db8affdf7c15b4c3ace
rsm-akotha <akotha@ucsd.edu>
2025-08-13 14:40:19 -0700

    wrapping spark.stop() within if and else so the glue_fact_events ends normally

--- transformations/scripts/glue_fact_events.py
+++ transformations/scripts/glue_fact_events.py
@@ -36,32 +36,31 @@ if not latest_file_key:
     print("No new event file found. Skipping ETL run.")
     spark.stop()
     print("Moving to next step with skipped Glue job...")
-    sys.exit(0)  # Exit gracefully so Step Functions sees success
+else:
+    # Full S3 path to the latest JSON file
+    latest_file_s3_path = f"s3://{source_bucket}/{latest_file_key}"
+    # ====== Read Raw JSON from latest file ======
+    df_raw = spark.read.json(latest_file_s3_path, multiLine=True)
 
-# Full S3 path to the latest JSON file
-latest_file_s3_path = f"s3://{source_bucket}/{latest_file_key}"
-# ====== Read Raw JSON from latest file ======
-df_raw = spark.read.json(latest_file_s3_path, multiLine=True)
+    # ====== Flatten & Select Required Columns ======
+    df_fact_events = df_raw.select(
+        col("visitor_key").alias("visitor_key"),
+        col("event_key").alias("event_key"),
+        col("received_at").alias("received_at"),  
+        col("percent_viewed").alias("percent_viewed"),  
+        col("ip").alias("ip"),
+        col("country").alias("country"),
+        col("media_id").alias("media_id"),
+        col("media_url").alias("media_url")
+    ).withColumn("load_date", current_date())
 
-# ====== Flatten & Select Required Columns ======
-df_fact_events = df_raw.select(
-    col("visitor_key").alias("visitor_key"),
-    col("event_key").alias("event_key"),
-    col("received_at").alias("received_at"),  
-    col("percent_viewed").alias("percent_viewed"),  
-    col("ip").alias("ip"),
-    col("country").alias("country"),
-    col("media_id").alias("media_id"),
-    col("media_url").alias("media_url")
-).withColumn("load_date", current_date())
+    # ====== Write to Parquet in Process Folder ======
+    target_path = f"s3://{target_bucket}/processed/fact_events/"
+    df_fact_events.write \
+        .mode("append") \
+        .partitionBy("load_date") \
+        .parquet(target_path)
 
-# ====== Write to Parquet in Process Folder ======
-target_path = f"s3://{target_bucket}/processed/fact_events/"
-df_fact_events.write \
-    .mode("append") \
-    .partitionBy("load_date") \
-    .parquet(target_path)
+    print(f"Successfully wrote fact_events data to {target_path}")
 
-print(f"Successfully wrote fact_events data to {target_path}")
-
-spark.stop()
+    spark.stop()
